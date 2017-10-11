59c0df037f8e976c847072355763e0e83301d765
esteban1234 <tva_libra@hotmail.com>
2017-10-11 18:10:20 -0500

    Pagina lista

---
+++ index.php
@@ -0,0 +1,347 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="utf-8">
+    
+    <link rel="icon" type="image/png" href="img/favicon.png" />
+    <title>CORPORATIVO DOCENTALIA</title>
+   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <META NAME="Title" content="CORPORATIVO DOCENTALIA">
+    <META NAME="Author" CONTENT="[http://docentalia.com/]"/>
+    <META name="Description" content="Venta de gasolina y diésel, Refinación de petróleo, Producción, elaboración de combustibles automotrices. Fabricación o producción de combustibles fósiles. Fabricación de motores de combustión interna, turbinas y transmisiones, Fabricación de otras partes para vehículos automotrices."/>
+   <META NAME="Keywords" CONTENT="Venta de gasolina y diésel, Refinación de petróleo, Producción, elaboración de combustibles automotrices. Fabricación o producción de combustibles fósiles. Fabricación de motores de combustión interna, turbinas y transmisiones, Fabricación de otras partes para vehículos automotrices."/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/iconos.min.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/responsivo.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="js/general.js"></script>
+
+
+
+    <script src="js/smooth-scroll.min.js"></script>
+    <script>
+      smoothScroll.init({
+      selector: '[data-scroll]',
+      selectorHeader: null,
+      speed: 2000,
+      easing: 'easeInOutCubic',
+      offset: 0,
+      callback: function ( anchor, toggle ) {}
+      });
+    </script>
+
+  </head>
+  <body>
+
+
+<div class="bannerh" id="INICIO">
+  <div class="fondo">
+    <header>
+      <figure class="logo pull-right">
+        <img src="img/docentalia.png" class="img-responsive" alt="DENNOR">
+      </figure>
+      <nav class=" menu pull-left">
+        <ul>
+          <a class="selector" id="select" data-scroll href="#INICIO" aria-pressed="true">INICIO</a>
+          <a class="selector" data-scroll href="#NOSOTROS" aria-pressed="true">NOSOTROS</a>
+          <a class="selector" data-scroll href="#SERVICIOS" aria-pressed="true">SERVICIOS</a>
+          <a class="selector" data-scroll href="#CONTACTO" aria-pressed="true">CONTACTO</a>
+        </ul>
+      </nav>
+    </header>
+    
+    <p class="text-banner"> Empresa Socialmente Responsable (ESR); <br> eficiente y competitiva en venta de gasolina y diesel</p>
+    <div style="text-align: center; margin-top: 5%; margin-bottom: 5%;">
+      <a class="btn btn-primary btn-outline btn-lg btn-color" data-scroll href="#CONTACTO">CONTACTANOS</a>
+    </div>
+  </div>
+</div>
+
+
+
+  <span class="ir-arriba"><i class="fa fa-arrow-up" aria-hidden="true"></i></span>
+
+<section class="seccion1" id="NOSOTROS">
+        <div class="row">
+          <div class="col-md-6 imagen">
+            
+          </div>
+          <div class="col-md-4 col-md-offset-1" >
+            <h1 class="tit">Nuestra <b style="color: #358983;">EMPRESA</b></h1>
+            <h4>CORPORATIVO DOCENTALIA S.A. DE C.V.</h4>
+            <p class="texto">Somos una empresa con servicios de venta de gasolina y diésel, refinación de petróleo y elaboración de combustibles automotrices. <br><br>
+            Nuestro objetivo principal es satisfacer la demanda de nuestros clientes, para lo cual contamos con la infraestructura más moderna en almacenaje, logística y personal altamente capacitado en el manejo y operación de todo tipo de combustibles. <br><br>
+            Entendemos los retos de las empresas de México y ponemos a su disposición herramientas y tecnologías que le permitan tener mayor control sobre sus consumos y manejos de combustible.</p>
+          </div>
+        </div>
+
+        <div class="section seccion11">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            
+            <h1 class="tit2"><b style="color: #358983;">Políticas</b></h1>
+            <!-- <h4>CORPORATIVO DOCENTALIA S.A. DE C.V.</h4> -->
+            <br><br>
+            <p class="texto">Somos una Empresa Socialmente Responsable (ESR), eficiente y competitiva en el area de los combustibles, nuestros servicios son de calidad preocupándonos en ofrecer al cliente el suministro de combustible en tiempo y forma, procurando su satisfacción y confianza; cuidando el Medio Ambiente, la Seguridad y Salud de nuestros trabajadores en el proceso de: Suministro de Diésel y Gasolina.<br><br>
+            Proporcionados con tecnología de vanguardia, personal calificado y comprometido en la prevención de la contaminación, de las lesiones y enfermedades, el cumplimiento de los requisitos legales y reglamentarios aplicables y con la mejora continua a través del cumplimiento de las Normas Internacionales 
+            </p>
+            <figure class="">
+                <img src="img/docentalia.png" class="img-responsive" alt="DENNOR">
+            </figure>
+          </div>
+          <div class="col-md-6">
+                <div class="row tit2">
+                  <div class="col-md-2">
+                    <i class="fa fa-check-square-o fa-5x" aria-hidden="true"></i>
+                  </div>
+                  <div class="col-md-10">
+                    <h3 class="text-left">Misión</h3>
+                    <p class="text-left texto">Servir al cliente en la venta de combustibles, excediendo sus expectativas de atención y servicio, con rapidez, amabilidad y confiabilidad realizado con un equipo humano altamente preparado, respetuoso, orientado a servir y generar valor para el cliente, nuestros colaboradores, los accionistas y la comunidad.</p>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-2">
+                    <i class="fa fa-eye fa-5x" aria-hidden="true"></i>
+                  </div>
+                  <div class="col-md-10">
+                    <h3 class="text-left">Visión</h3>
+                    <p class="text-left texto">Ser la mejor opción en el suministro de combustible, afianzando la fidelidad de nuestros clientes mediante el mejor trato, atención personalizada, la mayor calidad en el servicio, la generación de valores con el mejor equipo humano y tomando conciencia del cuidado del medio ambiente.</p>
+                  </div>
+                </div>
+                <div class="row">
+                  <div class="col-md-2">
+                    <i class="fa fa-retweet fa-5x" aria-hidden="true"></i>
+                  </div>
+                  <div class="col-md-6">
+                    <h3 class="text-left">Valores</h3>
+                    <p class="text-left texto">
+                      * Seguridad <br>
+                      * Calidad <br>
+                      * Servicio al Cliente <br>
+                      * Confianza <br>
+                      * Responsabilidad <br>
+                      * Conciencia Ecológica <br>
+                      * Responsabilidad social
+                    </p>
+                  </div>
+                </div>
+          </div>
+        </div>
+      </div>
+    </div>
+</section>
+
+
+<section class="seccion2" id="SERVICIOS">
+  <h1 class="text-center"><i class="fa fa-briefcase" aria-hidden="true"></i> Nuestros <b style="color: #358983;">SERVICIOS</b></h1>
+  <p class="text-center">Ponemos a su disposición nuestra amplia experiencia y asesoramiento en cuanto a la venta de combustible:</p> <br><br><br>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <figure class="imagens">
+              
+            </figure>
+            <h2 class="text-center">ALMACENAMIENTO</h2>
+            <p></p>
+          </div>
+          <div class="col-md-6">
+            <figure class="imagens2">
+              
+            </figure>
+            <h2 class="text-center">TRANSPORTE TERRESTRE</h2>
+            <p></p>
+          </div>
+        </div> <br><br>
+        <div class="row">
+          <div class="col-md-6">
+            <figure class="imagens3">
+              
+            </figure>
+            <h2 class="text-center">Producción, elaboración de combustibles automotrices.</h2>
+            <p></p>
+          </div>
+          <div class="col-md-6">
+            <figure class="imagens4">
+              
+            </figure>
+            <h2 class="text-center">Fabricación de motores de combustión interna, turbinas y transmisiones</h2>
+            <p></p>
+          </div>
+        </div>
+      </div>
+      <br><br>
+      <div class="container banners">
+        <div class="cont">
+           <h1 class="text-center">Producto En <b style="color: #358983;">VENTA</b></h1>
+             <p class="text-center">Ponemos a su disposición los siguientes productos:</p>
+         </div> <br><br><br>
+         <div class="container">   
+    <div class="pricing-grids">
+      <div class="col-md-4 pric-grid effect">
+        <div class="pric-top">
+          <span>1</span>                      
+          <h4>Pemex Diesel</h4>
+        </div>        
+        <div class="pric-list">
+          <ul>
+            <li>Combustible especializado para vehículos automotrices Diésel</li>
+            </ul>
+        </div>
+      </div>      
+      <div class="col-md-4 pric-grid effect">
+        <div class="pric-top">
+          <span>2</span>                      
+          <h4>Diesel Industrial Bajo Azufre</h4>
+        </div>
+        <div class="pric-list">
+          <ul>
+            <li>Combustible especialmente indicado para obtener el máximo rendimiento en maquinaria de uso industrial, agrícola y calderas.</li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-4 pric-grid effect">
+        <div class="pric-top">
+          <span>3</span>                      
+          <h4>Petroleo</h4>
+        </div>
+        <div class="pric-list">
+          <ul>
+            <li>Se utiliza Como combustible doméstico e industrial, Como carburante y lubricante, Como materia prima básica en la industria petroquímica, etc.</li>
+          </ul>
+        </div>
+        </div>
+          <div class="clearfix"> </div>
+      </div>
+         </div>
+      </div>
+</section>
+
+<section class="seccion3" id="CONTACTO">
+  <h1 class="text-center"><i class="fa fa-phone" aria-hidden="true"></i><b style="color: #358983;"> CONTACTO</b></h1>
+  <p class="text-center" style="width: 80%;margin: 0 auto;">Podrás dejar tu consulta, comentario, sugerencia o lo que consideres conveniente en relación a nuestros servicios y nuestro equipo de especialistas se encargará de responderte en la mayor brevedad posible.</p> <br><br><br>
+      <!-- <div class="container">
+        <div class="row">
+        </div>
+      </div> -->
+      <div class="container animated fadeIn">
+
+  <div class="row">
+    <div class="col-sm-12" id="parent">
+      <div class="col-sm-6">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d232.78788626644382!2d-89.62302374620555!3d21.008417859565466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f5676a86061e939%3A0x700a62b9e77240f2!2sCalle11+335%2C+Fraccionamiento+del+Nte%2C+97120+M%C3%A9rida%2C+Yuc.!5e0!3m2!1ses-419!2smx!4v1507763194130" width="100%" height="320px" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+
+      <div class="col-sm-6">
+        <form action="form.php" class="contact-form" method="post">
+  
+            <div class="form-group">
+              <input type="text" class="form-control" id="name" name="nm" placeholder="Nombre Completo" required="">
+            </div>
+        
+        
+            <div class="form-group form_left">
+              <input type="email" class="form-control" id="email" name="em" placeholder="Email" required="">
+            </div>
+        
+          <div class="form-group">
+               <input type="text" class="form-control" id="phone" maxlength="10" placeholder="Numero teléfonico" required="">
+          </div>
+          <div class="form-group">
+          <textarea class="form-control textarea-contact" rows="5" id="comment" name="FB" placeholder="Escribe tu comentario" required=""></textarea>
+          <br>
+            <button class="btn btn-default btn-send"> <span class="fa fa-send"></span> Enviar </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <div class="container second-portion">
+  <div class="row">
+        <!-- Boxes de Acoes -->
+      <div class="col-md-3">
+      <div class="box">             
+        <div class="icon">
+          <div class="image"><i class="fa fa-envelope" aria-hidden="true"></i></div>
+          <div class="info">
+            <h3 class="title">CORREO</h3>
+            <p>
+              <i class="fa fa-envelope" aria-hidden="true"></i> &nbsp info@docentalia.com
+              <br>
+              <br>
+              <i class="fa fa-globe" aria-hidden="true"></i> &nbsp www.docentalia.com
+            </p>
+          
+          </div>
+        </div>
+        <div class="space"></div>
+      </div> 
+    </div>
+      
+        <div class="col-md-3">
+      <div class="box">             
+        <div class="icon">
+          <div class="image"><i class="fa fa-mobile" aria-hidden="true"></i></div>
+          <div class="info">
+            <h3 class="title">CONTACTO</h3>
+              <p>
+              <i class="fa fa-mobile" aria-hidden="true"></i> &nbsp PENDIENTE
+            </p>
+          </div>
+        </div>
+        <div class="space"></div>
+      </div> 
+    </div>
+      
+        <div class="col-md-3">
+      <div class="box">             
+        <div class="icon">
+          <div class="image"><i class="fa fa-map-marker" aria-hidden="true"></i></div>
+          <div class="info">
+            <h3 class="title">DIRECCION</h3>
+              <p>
+               <i class="fa fa-map-marker" aria-hidden="true"></i> &nbsp C.11 #335-A POR 60 Y 38 COL. FRACC. DEL NORTE, MERIDA, YUC. C.P.97120
+            </p>
+          </div>
+        </div>
+        <div class="space"></div>
+      </div> 
+    </div>        
+    
+     <div class="col-md-3">
+      <div class="box">             
+        <div class="icon">
+          <div class="image"><i class="fa fa-clock-o" aria-hidden="true"></i></div>
+          <div class="info">
+            <h3 class="title">HORARIO</h3>
+              <p>
+               <i class="fa fa-clock-o" aria-hidden="true"></i> &nbsp Lunes a Viernes de 9 am a 6 pm
+          </div>
+        </div>
+        <div class="space"></div>
+      </div> 
+    </div> 
+      
+  </div>
+</div>
+
+</div>
+</section>
+
+<footer>
+<p>Derechos de autor 2017 © | CORPORATIVO DOCENTALIA S.A. DE C.V.</p>
+</footer>
+
+    <script src="js/sendCORREO.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.3.7/js/tether.min.js" integrity="sha384-XTs3FgkjiBgo8qjEjBk0tGmf3wPrWtA6coPfQDfFEY8AnYJwjalXCiosYRBIBZX8" crossorigin="anonymous"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.js"></script>
+    <script src="js/arriba.js"></script>
+    <script src="js/bootstrap.js"></script>
+    <script src="js/menu.js"></script>
+  </body>
+</html>
