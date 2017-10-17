a7a635f9ecb5e44d20db6f02895f4ad26d9892cd
esteban1234 <tva_libra@hotmail.com>
2017-10-17 18:14:50 -0500

    metas

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html lang="es">
   <head>
     <meta charset="utf-8">
-    
+    <meta name="google-site-verification" content="XlQF3bjzprndHRPdsHQ3_911PMjTSBWmL7uBB6Ecl6E" />
     <link rel="icon" type="image/png" href="img/favicon.png" />
     <title>CORPORATIVO DOCENTALIA</title>
    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
